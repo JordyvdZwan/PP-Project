73349aad83469072bde3fd6ce99fe9e6bd52a9e4
Jordy van der Zwan <jordyvdzwan@hotmail.com>
2017-07-06 13:34:50 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	testSrc/SprockellTest.java

--- testSrc/RunTest.java
+++ testSrc/RunTest.java
@@ -1,22 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by Jordy van der Zwan on 06-Jul-17.
  */
 public class RunTest {
-    /*-----------------------------------------------------------
-                           Attributes
-     -----------------------------------------------------------*/
-    
-    
-    
-    /*-----------------------------------------------------------
-                            Methods
-     -----------------------------------------------------------*/
-    
-    
-    
-    /*-----------------------------------------------------------
-                       Getters and Setters
-     -----------------------------------------------------------*/
 
 
+
+    @Test
+    public void before() {
+        try {
+            ProcessBuilder builder = new ProcessBuilder(
+                    "cmd.exe", "/c", "cd \"C:\\Projects\\Project - Programming Paradigms Compiler Project\\PP-Compiler-Project\\resources\\out\" & ghci & :l output.hs & main");
+            builder.redirectErrorStream(true);
+            Process p = builder.start();
+            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            while (true) {
+                line = r.readLine();
+                if (line == null) {
+                    break;
+                }
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
